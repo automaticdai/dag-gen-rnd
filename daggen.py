036c638855cc4af5ee00c72ccfe9c6618b78a538
automaticdai <automatic.dai@gmail.com>
2020-02-12 02:50:45 -0800

    PyQt5 support

--- daggen.py
+++ daggen.py
@@ -9,45 +9,70 @@ import matplotlib.image as mpimg
 
 import json
 
-# read configurations
-with open('config.json', 'r') as f:
-    array = json.load(f)
-
-print (array["tg"])
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox
 
 
-# initial a new graph
-G = nx.DiGraph()
+def on_button_clicked():
+    gen()
 
-# add nodes
-G.add_node('n_1',rank=0)
-G.add_nodes_from(['n_2','n_3','n_4'],style='filled',fillcolor='red',shape='diamond')
-G.add_nodes_from(['E','F','G'])
-G.add_nodes_from(['H'],label='target')
 
-# add edges
-G.add_edge('n_1','n_2',arrowsize=2.0)
-G.add_edge('n_1','n_3',penwidth=2.0)
-G.add_edge('n_1','n_4')
-G.add_edges_from([('B','E'),('B','F')],color='blue')
-G.add_edges_from([('C','E'),('C','G')])
-G.add_edges_from([('D','F'),('D','G')])
-G.add_edges_from([('E','H'),('F','H'),('G','H')])
+def gui_init():
+    app = QApplication([])
+    window = QWidget()
+    layout = QVBoxLayout()
+    button = QPushButton('Generate')
+    layout.addWidget(button)
+    layout.addWidget(QPushButton('Exit'))
+    window.setLayout(layout)
+    window.show()
+    button.clicked.connect(on_button_clicked)
+    app.exec_()
 
-# set graph properties
-G.graph['graph']={'rankdir':'TD'}
-G.graph['node']={'shape':'circle'}
-G.graph['edges']={'arrowsize':'2.0'}
 
-# layout graph
-A = to_agraph(G)
-print(A)
-A.layout('dot')
+def gen():
+    # initial a new graph
+    G = nx.DiGraph()
 
-# plot graph
-filename = 'output/abcd.png'
-A.draw(filename)
+    # add nodes
+    G.add_node('n_1',rank=0)
+    G.add_nodes_from(['n_2', 'n_3', 'n_4'], style='filled', fillcolor='red', shape='diamond')
+    G.add_nodes_from(['E', 'F', 'G'])
+    G.add_nodes_from(['H'], label='target')
 
-img = mpimg.imread(filename)
-plt.imshow(img)
-plt.show()
+    # add edges
+    G.add_edge('n_1', 'n_2', arrowsize=2.0)
+    G.add_edge('n_1', 'n_3', penwidth=2.0)
+    G.add_edge('n_1', 'n_4')
+    G.add_edges_from([('B', 'E'), ('B', 'F')], color='blue')
+    G.add_edges_from([('C', 'E'), ('C', 'G')])
+    G.add_edges_from([('D', 'F'), ('D', 'G')])
+    G.add_edges_from([('E', 'H'), ('F', 'H'), ('G', 'H')])
+
+    # set graph properties
+    G.graph['graph'] = {'rankdir': 'TD'}
+    G.graph['node'] = {'shape': 'circle'}
+    G.graph['edges'] = {'arrowsize': '2.0'}
+
+    # layout graph
+    A = to_agraph(G)
+    print(A)
+    A.layout('dot')
+
+    # plot graph
+    filename = 'output/abcd.png'
+    A.draw(filename)
+
+    img = mpimg.imread(filename)
+    plt.imshow(img)
+    plt.show()
+
+
+if __name__ == "__main__":
+    # load configurations
+    with open('config.json', 'r') as f:
+        array = json.load(f)
+
+    print(array["tg"])
+
+    # initialize GUI
+    gui_init()
