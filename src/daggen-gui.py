59938a848de5e9d9ea909687ff3db77241871ce1
automaticdai <automatic.dai@gmail.com>
2022-11-28 21:37:51 +0000

    add QMainWindow, menubar and statusbar

--- src/daggen-gui.py
+++ src/daggen-gui.py
@@ -10,6 +10,7 @@
 
 import os, logging
 
+import sys
 import networkx as nx
 from   networkx.drawing.nx_agraph import graphviz_layout, to_agraph
 import pygraphviz as pgv
@@ -20,33 +21,64 @@ import matplotlib.image as mpimg
 import json
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QPushButton,
-                             QSlider, QFormLayout, QMessageBox, QComboBox,
-                             QLineEdit, QCheckBox)
+from PyQt5.QtWidgets import (QMainWindow, QApplication, QStatusBar, QWidget, QLabel,
+                             QPushButton, QSlider, QFormLayout, QMessageBox, 
+                             QComboBox, QLineEdit, QCheckBox, QMenu, QAction)
 
 from random import seed, randint, random
 
 import rnddag
 
 
-class GUI:
+class daggen_gui(QMainWindow):
     def __init__(self):
-        # create application
-        app = QApplication([])
+        super().__init__()
+        self.initUI()
 
-        # load default configuration
+    def initUI(self):
+        # config menu
+        menubar = self.menuBar()
 
+        loadAct = QAction('Load Config', self)
+        saveAct = QAction('Save Config', self)
 
+        fileMenu = menubar.addMenu('File')
+
+        impMenu = QMenu('Import', self)
+        impAct = QAction('Import from file', self)
+        impMenu.addAction(impAct)
+
+        quitExt = QAction('Quit', self)
+
+        fileMenu.addAction(loadAct)
+        fileMenu.addAction(saveAct)
+        fileMenu.addMenu(impMenu)
+        fileMenu.addAction(quitExt)
+
+        # config status bar
+        self.statusBar = QStatusBar()
+        self.setStatusBar(self.statusBar)
+        self.statusBar.showMessage("Ready")
+
+        # config UI layout
+        self.gui()
+
+        # start the window
+        self.setGeometry(500, 500, 500, 500)
+        self.setWindowTitle('dag-gen-rnd: Randomised DAG Generator')
+        self.show()
+
+    def gui(self):
         # create widgets
         # myFont = Qt.QFont()
         # myFont.setBold(True)
         # self.label.setFont(myFont)
 
-        label = QLabel("dag-gen-rnd: Randomised DAG Generator")
+        label = QLabel("Parameter Configurations:")
 
         opt_alogrithm = QComboBox()
-        opt_alogrithm.addItem("Default")
-        opt_alogrithm.addItem("Nested-Fork Join (NFJ)")
+        opt_alogrithm.addItem("Default (rnd)")
+        opt_alogrithm.addItem("Nested-Fork Join (nfj)")
 
         check_conditional = QCheckBox()
         check_mcs_dag = QCheckBox()
@@ -56,9 +88,10 @@ class GUI:
         edit_critical_max = QLineEdit()
         edit_critical_min = QLineEdit()
         edit_pc = QLineEdit()
+        edit_mcs = QLineEdit()
 
-        #slider_pc = QSlider(Qt.Horizontal)
-        #slider_pc.setRange(0, 100)
+        # slider_pc = QSlider(Qt.Horizontal)
+        # slider_pc.setRange(0, 100)
 
         button_gen_conf = QPushButton('Generate Configuration')
         button_gen = QPushButton('Generate DAGs')
@@ -68,11 +101,11 @@ class GUI:
         formLayout.addRow(label)
 
         formLayout.addRow("&Algorithm:", opt_alogrithm)
-        formLayout.addRow("&Maximum Parallelism <font color='blue'>>=1</font>:", edit_parallism)
-        formLayout.addRow("Critical Path (min) <font color='blue'>>=3</font>:", edit_critical_min)
-        formLayout.addRow("Critical Path (max) <font color='blue'>>=3</font>:", edit_critical_max)
+        formLayout.addRow("&Maximum parallelism <font color='blue'>>=1</font>:", edit_parallism)
+        formLayout.addRow("Min critical path length <font color='blue'>>=3</font>:", edit_critical_min)
+        formLayout.addRow("Max critical path length <font color='blue'>>=3</font>:", edit_critical_max)
         formLayout.addRow("p(Connnection) <font color='blue'>[0,1]</font>:", edit_pc)
-        formLayout.addRow("&Mixed-Criticality DAG?", check_mcs_dag)
+        formLayout.addRow("&Mixed-criticality DAG?", check_mcs_dag)
         formLayout.addRow("&Conditional DAG?", check_conditional)
 
         formLayout.addRow(button_gen_conf)
@@ -83,23 +116,21 @@ class GUI:
         edit_critical_max.setText("7")
         edit_critical_min.setText("3")
         edit_pc.setText("0.5")
-
-        # create window
-        window = QWidget()
-        window.setLayout(formLayout)
-        window.show()
+        edit_mcs.setText("0.5")
 
         # set signal / slots
         button_gen.clicked.connect(self.event_on_button_gen_clicked)
 
-        # stary application
-        app.exec_()
-
+        # create widget in the main window
+        widget = QWidget()
+        widget.setLayout(formLayout)
+        self.setCentralWidget(widget)
 
     def event_on_button_gen_clicked(self):
-        G = rnddag.gen()
-        rnddag.plot(G)
-        rnddag.save(G)
+        G = rnddag.DAG()
+        G.gen("rnd")
+        G.save()
+        G.plot()
 
 
 # Main function
@@ -111,11 +142,7 @@ if __name__ == "__main__":
     # fix random seed
     # seed(rnd_seed)
 
-    #print(array["tg"])
-
     # initialize the GUI
-    gui = GUI()
-
-    # for test only
-    #event_on_button_gen_clicked()
-    #plt.show()
+    app = QApplication(sys.argv)
+    win = daggen_gui()
+    sys.exit(app.exec_())
